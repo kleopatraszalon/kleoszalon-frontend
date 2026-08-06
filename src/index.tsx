da9fe6fb9f1e57bdc4e27a828428833e732c01c7
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-06 14:58:57 +0200

    Update src/index.tsx for Kleopatra brand alignment

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import App from "./App";
 import "./styles/kleo-theme.css";
 import "./styles/vir-altegio.css";
 import "./App.css";
+import "./styles/kleopatra-brand-2026.css";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Hiányzik a #root elem az index.html-ből");
