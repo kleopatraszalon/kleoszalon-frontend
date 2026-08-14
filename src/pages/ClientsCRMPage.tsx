3c44fd58a6e310cbc60351d849fe9a23ce07785e
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-14 11:16:06 +0200

    CRM final: client data-quality and versioned forms workspaces on latest main

--- src/pages/ClientsCRMPage.tsx
+++ src/pages/ClientsCRMPage.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import ClientsCRMCore from "./ClientsCRMCore";
+import ClientDuplicateReviewPage from "./ClientDuplicateReviewPage";
+import ClientFormsVersionPage from "./ClientFormsVersionPage";
 import ClientGovernanceLauncher from "./clients/ClientGovernanceLauncher";
 
 export default function ClientsCRMPage(){
+  const { view }=useParams();
+  if(view==="duplicate-review") return <ClientDuplicateReviewPage/>;
+  if(view==="forms") return <ClientFormsVersionPage/>;
   return <>
     <ClientsCRMCore/>
     <ClientGovernanceLauncher/>
