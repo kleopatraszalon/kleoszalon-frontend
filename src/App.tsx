0df4cdc93a3fe3060477e59c196b8954ab6b4d46
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-28 11:46:53 +0200

    Apply global VIR font scale

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./styles/kleo-theme.css";
+import "./styles/vir-font-scale.css";
 import BrandLoadingScreen from "./components/BrandLoadingScreen";
 import { applicationRoutes } from "./routing/routes";
 
