ee79db87f9c24188d8cd97fcb7d14b9b6dd3d534
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-30 15:06:55 +0200

    feat: register VIR P8 page

--- src/routing/routePages.ts
+++ src/routing/routePages.ts
@@ -58,6 +58,7 @@ export const VirP4Page = lazy(() => import("../pages/VirP4Page"));
 export const VirP5Page = lazy(() => import("../pages/VirP5Page"));
 export const VirP6Page = lazy(() => import("../pages/VirP6Page"));
 export const VirP7Page = lazy(() => import("../pages/VirP7Page"));
+export const VirP8Page = lazy(() => import("../pages/VirP8Page"));
 export const VirRevenueLeakagePage = lazy(() => import("../pages/VirRevenueLeakagePage"));
 export const VirStaffDetailPage = lazy(() => import("../pages/VirStaffDetailPage"));
 export const VirServiceDetailPage = lazy(() => import("../pages/VirServiceDetailPage"));
